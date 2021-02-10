9000f670ead8de18250e65d620a036f1ccd0350f
Krystian Piotrowski <piotrowski24h@interia.pl>
2021-02-10 02:42:22 +0100

    6.2

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/Calculator.java
@@ -0,0 +1,24 @@
+
+package com.kodilla.testing;
+
+import com.kodilla.testing.TestingMain;
+
+ public class TestingMain {
+
+     public static void main(String[] args) {
+         Calculator calculator = new Calculator();
+
+
+         private void add() {
+             int result = add(5,10);
+             System.out.println(result);
+         }
+         private int add(int a, int b) {
+             return a+b;
+         }
+     }
+ }
+
+
+
+
