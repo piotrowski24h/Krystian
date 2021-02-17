28357cb3217b69e881102cb88b4e88abf1434318
Krystian Piotrowski <piotrowski24h@interia.pl>
2021-02-17 13:58:57 +0100

    III

--- kodilla-testing/src/main/java/com/kodilla/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/TestingMain.java
@@ -1,2 +1,18 @@
-package com.kodilla;public class TestingMain {
+package com.kodilla;
+import com.kodilla.testing.calculator.Calculator;
+
+
+public class TestingMain {
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int addResult = calculator.add(20, 120);
+        int subResult = calculator.subtract(200, 150);
+        if ((addResult == 199) && (subResult == 50)) {
+            System.out.println("test ok");
+        } else{
+            System.out.println("Error!");
+        }
+
+    }
 }
