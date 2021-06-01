b27ce4948cc9cef8a4cf6a7dfbe98bf840a7da4e
Krystian Piotrowski <piotrowski24h@interia.pl>
2021-06-01 15:32:27 +0200

    My world 7.4

--- kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
@@ -1,5 +1,23 @@
 package com.kodilla.stream.world;
 
-public class World {
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
 
+public class World {
+ private final Set<Continent> continents = new HashSet<>();
+
+ public World() {
+ }
+
+ public BigDecimal getPeopleQuantity() {
+  return continents.stream()
+          .flatMap(continent -> continent.getCountries().stream())
+          .map(Country::getPeopleQuantity)
+          .reduce(BigDecimal.ZERO, (sum, current) -> sum = sum.add(current));
+ }
+
+ public Set<Continent> getContinents() {
+  return continents;
+ }
 }
