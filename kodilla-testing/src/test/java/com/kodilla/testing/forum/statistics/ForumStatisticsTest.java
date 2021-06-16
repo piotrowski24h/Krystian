d88f31b91cccc7efae3c7979a267f2329397a576
Krystian Piotrowski <piotrowski24h@interia.pl>
2021-06-16 16:43:35 +0200

    statystyka uzytkowników 6.7

--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticsTest.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticsTest.java
@@ -1,13 +1,16 @@
 package com.kodilla.testing.forum.statistics;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class ForumStatisticsTest {
     private Statistics statistics;
@@ -23,14 +26,14 @@ class ForumStatisticsTest {
     void assertThatPostDoesntEXIST(){
         when(statistics.postsCount()).thenReturn(0);
         forumStatistics.calculateAdvStatistics(statistics);
-
+         //then
         assertEquals(0,forumStatistics.getTotalPostsNumber());
     }
     @Test
     void testWhilePostNumberIsOne(){
         when(statistics.postsCount()).thenReturn(1000);
         forumStatistics.calculateAdvStatistics(statistics);
-
+//then
         assertEquals(1000,forumStatistics.getTotalPostsNumber());
     }
 
@@ -38,6 +41,7 @@ class ForumStatisticsTest {
     void assertThatPostNumberIsZero(){
         when(statistics.commentsCount()).thenReturn(0);
         forumStatistics.calculateAdvStatistics(statistics);
+        //then
         assertEquals(0,forumStatistics.getTotalCommentsNumber());
 
     }  @Test
@@ -45,6 +49,7 @@ class ForumStatisticsTest {
         when(statistics.commentsCount()).thenReturn(10);
         when(statistics.postsCount()).thenReturn(1);
         forumStatistics.calculateAdvStatistics(statistics);
+        //then
         assertTrue(forumStatistics.getTotalCommentsNumber() > forumStatistics.getTotalPostsNumber());
     }
 
@@ -53,6 +58,7 @@ class ForumStatisticsTest {
         when(statistics.commentsCount()).thenReturn(1);
         when(statistics.postsCount()).thenReturn(10);
         forumStatistics.calculateAdvStatistics(statistics);
+        //then
         assertTrue(forumStatistics.getTotalCommentsNumber() < forumStatistics.getTotalPostsNumber());
     }
 
@@ -61,20 +67,40 @@ class ForumStatisticsTest {
         List<String> list = mock(statistics.userNames().getClass());
         when(list.size()).thenReturn(0);
         forumStatistics.calculateAdvStatistics(statistics);
+        //then
         assertEquals(0,forumStatistics.getTotalUsersNumber());
     }
 
-    @Test
+    @Test   // - zmiana koncepcji
     void assertThatUsersAre100(){
-        List<String> strings = statistics.userNames();
-        verify(strings).add("asd");
-        when(statistics.userNames()).thenReturn(strings);
+        List<String> topUsers =new ArrayList<>();// inaczej niz string rzeczywiste dane. (string.add("Wojtek");
+        topUsers.add("Wojtek");
+        topUsers.add("Adam");
+        topUsers.add("Ola");
+        topUsers.add("Adrian");
 
 
-        System.out.println(strings);
+        //verify(string).add("abc");
+        when(statistics.userNames()).thenReturn(topUsers);
+        forumStatistics.calculateAdvStatistics(statistics);
+  //then
+        Assertions.assertEquals(4 ,forumStatistics.getTotalUsersNumber());
+
+        System.out.println(topUsers);
 
     }
-
+//    @Test
+//    void assertThatUsersAre100(){
+//        double commentPerUser= 100;
+//        private int totalPostsNumber = 100;
+//        List <String> usersList = new ArrayList<String>()
+//                for (int i = 0; i <100; i++) {
+//                    usersList.add("Users" +i);
+//                }
+//  when(statistics.userNames()).thenReturn(usersList);
+//          //then
+//        int
+//    }
 
 
 
